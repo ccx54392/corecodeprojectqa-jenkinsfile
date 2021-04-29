e2fa90e5a49cebed77748774af24731f36c831d0
Pablo <ccx54392@gmail.com>
2021-04-28 19:02:23 -0600

    first commit

---
+++ pipelineExecution/JenkinsFile.groovy
@@ -0,0 +1,55 @@
+def checkoutApi = { String subFolder ->
+    checkout([
+            $class      : "GitSCM",
+            branches    : [[name: "*/main"]],
+            extensions  : [[ $class: "RelativeTargetDirectory",
+                             relativeTargetDir: subFolder
+                           ]],
+            userRemoteConfigs: [[
+                                        url : 'git@github.com:ccx54392/corecodeproject_api_.git',
+                                        credentialsId: "myprivatesshkey"
+                                ]]
+    ])
+}
+
+def checkoutTesting = { String subFolder ->
+    checkout([
+            $class      : "GitSCM",
+            branches    : [[name: "*/main"]],
+            extensions  : [[ $class: "RelativeTargetDirectory",
+                             relativeTargetDir: subFolder
+                           ]],
+            userRemoteConfigs: [[
+                                        url : 'git@github.com:JosueDa/corecodeproject_testing.git',
+                                        credentialsId: "myprivatesshkey"
+                                ]]
+    ])
+}
+
+def runner = { commandToExecute -> isUnix() ? sh(commandToExecute) : bat(commandToExecute) }
+
+
+unitTest:{
+    stage("Unit Testing"){
+        node('MyNewNode'){
+            checkoutApi("tests")
+            runner 'cd tests && mvn test -Dgroups=unitTest'
+        }
+    }
+}
+IntegrationTest:{
+    stage("Integration Testing"){
+        node('MyNewNode'){
+            checkoutTesting("tests")
+            runner 'cd tests && mvn test -Dgroups=integrationTest -DapiUrl=${apiUrl}'
+        }
+    }
+}
+SystemTest:{
+    stage("System or E2E Testing"){
+        node('MyNewNode'){
+            checkoutTesting("tests")
+            runner 'cd tests && mvn test -Dgroups=systemTest -DwebUrl=${webUrl} -Dbrowser=${browser}'
+        }
+    }
+}
